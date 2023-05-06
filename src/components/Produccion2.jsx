9ed0995ded238a6a04ad8039b96f48bbf3b06d9c
laurafernandezv <laurafernandezgotadeleche@gmail.com>
2023-05-06 00:24:40 -0500

    se crean los componentes de Produccion1 Produccion2 Produccion3 Produccion4 Produccion5 Bio

---
+++ src/components/Produccion2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+
+const Produccion2 = () => {
+  return (
+    <section>
+        <Header/>
+    </section>
+  )
+}
+
+export default Produccion2
---
+++ src/components/Produccion4.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+
+const Produccion4 = () => {
+  return (
+    <section>
+        <Header/>
+    </section>
+  )
+}
+
+export default Produccion4
