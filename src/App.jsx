cb1446f5c7f613cc3bf47a067a1ea88894267e62
crissd1984 <cristiansandovalduque@gmail.com>
2023-05-05 22:23:13 -0500

    se agrega componente contacto con estilos se instala conexion servidor para envio de correo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Cargarimg from "./components/recursos/Cargarimg";
 import Gestionprod from "./components/recursos/Gestionprod";
 import Modificarbio from "./components/recursos/Modificarbio";
+import Contacto from "./components/recursos/Contacto";
 
 const router = createBrowserRouter([
    {
@@ -18,7 +19,11 @@ const router = createBrowserRouter([
       path: "/modificarbio",
       element: <Modificarbio />,
       errorElement: <h1>Pagina no encontrada</h1>
-   }
+   },
+     {
+      path:"/contacto",
+      element: <Contacto />,
+    },
 ]);
 function App() {
    return (
